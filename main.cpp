8ae4e0cda576380b3af7b07e2cae218ec53b6120
Ansh <anshku037@gmail.com>
2025-04-08 18:35:36 +0530

    second commit

--- main.cpp
+++ main.cpp
@@ -1,3 +1,12 @@
+
+// ChessIndia - GPLv3-licensed chess GUI
+// Copyright (C) 2024 Ansh Kumar
+// 
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
 #include <iostream>
 #include <cctype>
 #include <SFML/Graphics.hpp>
